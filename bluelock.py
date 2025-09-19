c4285478eae810acb9e32be29a3b507147ee2768
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2025-09-19 02:42:01 -0300

    first commit

---
+++ bluelock.py
@@ -0,0 +1,19 @@
+class Jogador:
+    def __init__(self, nome, arma, time):
+        self.nome = nome
+        self.arma = arma
+        self.time = time
+        
+    def exec(self):
+        print("Randola fod")
+
+class Forward(Jogador):
+    def exec(self):
+        print(f"Eu sou {self.nome}, sou um atacante, minha arma é {self.arma} e eu jogo no {self.time}.")
+
+
+jog1 = Forward("Yoichi Isagi", "Chute Direto e Metavisão", "Bastard München")
+jog2 = Forward("Michael Kaiser", "Kaiser Impact e Metavisão", "Bastard München")
+
+jog1.exec()
+jog2.exec()
