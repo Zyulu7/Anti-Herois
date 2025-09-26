443f8c428ce42b9d7d05606571e320a180b43d26
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2025-09-25 21:55:25 -0300

    Att 26_09_25

--- atvcar.py
+++ atvcar.py
@@ -1,5 +1,6 @@
 class Carro:
-    def __init__(self, marca, modelo, ano, cor, velocidade):
+    def __init__(self, marca, modelo, ano, cor):
+
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
@@ -7,11 +8,17 @@ class Carro:
         self.velocidade = 0
 
     def acelerar(self, valor):
-        self.velocidade += valor
-        print(f"{self.modelo} acelerou!")
-    
-    def frear(self, valor):
-        self.velocidade -= valor
-        print(f"{self.modelo} freou!")
 
-    
+        self.velocidade += valor
+        print(f"{self.modelo} acelerou para {self.velocidade} km/h!")
+
+    def frear(self, valor):
+        
+        if self.velocidade < 0:
+            self.velocidade = 0
+            print(f"{self.modelo} reduziu para {self.velocidade} km/h.")
+
+    def detalhes(self):
+        
+        return (f"{self.marca} {self.modelo} ({self.ano}) - "
+        f"Cor: {self.cor}, Velocidade: {self.velocidade} km/h")
